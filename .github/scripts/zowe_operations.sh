945e39f14a73b6db01d9d4d920242a5a97cbfcb6
Chico <chicokds@gmail.com>
2026-07-28 10:10:32 +0200

    Using npx zowe instead of zowe

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -3,15 +3,15 @@
 # Convert username to lowercase
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 # Check if directory exists, create if it doesn't
-if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
+if ! npx zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
     echo "Directory does not exist. Creating it..."
-    zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
+    npx zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
 else
     echo "Directory already exists."
 fi
 # Upload files
-zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive
+npx zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive
         --binary-files "cobol-check-0.2.9.jar"
 # Verify upload
 echo "Verifying upload:"
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
+npx zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
